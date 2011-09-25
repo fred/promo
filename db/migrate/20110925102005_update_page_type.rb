bc05f4440de82ba6c31f590a6f74f5f9e1a30432
Frederico Araujo <fred.the.master@gmail.com>
2011-09-25 17:39:34 +0700

    small CSS adjust

--- db/migrate/20110925102005_update_page_type.rb
+++ db/migrate/20110925102005_update_page_type.rb
@@ -1,6 +1,7 @@
 class UpdatePageType < ActiveRecord::Migration
   def self.up
-    Item.where(:type => nil).update_all(:type => "Page", :unread => false)
+    Item.where(:type => nil).update_all(:type => "Page")
+    Message.update_all(:unread => false)
   end
 
   def self.down
